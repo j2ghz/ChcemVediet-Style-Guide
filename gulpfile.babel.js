3bb5802cf64ef49865b5ca122e869b53b2acac1b
J2ghz <j2.00ghz@gmail.com>
2015-09-04 17:10:26 +0200

    apply updatefrom google web starter pack

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -123,7 +123,7 @@ gulp.task('styles', () => {
     'app/styles/**/*.scss',
     'app/styles/**/*.css'
   ])
-    .pipe($.changed('.tmp/styles', {extension: '.css'}))
+    .pipe($.newer({dest: '.tmp/styles', ext: '.css'}))
     .pipe($.sourcemaps.init())
     .pipe($.sass({
       precision: 10
