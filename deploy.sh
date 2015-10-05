f95a5a83ac38e23df6b11d920c649309761a4288
Jozef Hollý <j2ghz@users.noreply.github.com>
2015-10-05 07:05:18 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,3 +25,4 @@ git commit -m "Deploy to GitHub Pages"
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+rsync -av --progress . $CIRCLE_ARTIFACTS --exclude .git
